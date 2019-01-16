02d19693aa60de33407610c3533ba6b840c92a69
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2019-01-16 17:53:09 +0800

    http://www.helsinkitimes.fi/

---
+++ crawler-www.helsinkitimes.fi/contentDeal.py
@@ -0,0 +1,44 @@
+import re
+# 处理字符串文本内容
+def deal_content(content) :
+    # 将 h1 标签转成 p 标签
+    content = re.sub(r"h1>", "p>", content)
+    # 将 figcaption 标签转成 p 标签
+    content = re.sub(r"figcaption>", "p>", content)
+    # 将 p 标签规格化
+    content = content.replace("</p>", "<p>")
+    content = re.sub(r"<p.*?>", "<p>", content)
+    # 将 br 标签变为空格
+    content = re.sub(r"<br.*?>", " ", content)
+    content = content.replace("</br>", " ")
+    # 去除 a 开头的标签
+    content = re.sub(r"<a.*?>", "", content)
+    content = re.sub(r"</a.*?>", "", content)
+    # 去除 b 开头的标签
+    content = re.sub(r"<b.*?>", "", content)
+    content = re.sub(r"</b.*?>", "", content)
+    # 去除 e 开头的标签
+    content = re.sub(r"<e.*?>", "", content)
+    content = re.sub(r"</e.*?>", "", content)
+    # 去除 f 开头的标签
+    content = re.sub(r"<f.*?>", "", content)
+    content = re.sub(r"</f.*?>", "", content)
+    # 去除 i 开头的标签
+    content = re.sub(r"<i.*?>", "", content)
+    content = re.sub(r"</i.*?>", "", content)
+    # 去除 l 开头的标签
+    content = re.sub(r"<l.*?>", "", content)
+    content = re.sub(r"</l.*?>", "", content)
+    # 去除 s 开头的标签
+    content = re.sub(r"<s.*?>", "", content)
+    content = re.sub(r"</s.*?>", "", content)
+    # 去除 t 开头的标签
+    content = re.sub(r"<t.*?>", "", content)
+    content = re.sub(r"</t.*?>", "", content)
+    # 去除 u 开头的标签
+    content = re.sub(r"<u.*?>", "", content)
+    content = re.sub(r"</u.*?>", "", content)
+    # 去除多余的空格
+    content = re.sub(r"\s+", " ", content)
+    return content
+    
---
+++ crawler-www.helsinkitimes.fi/crawler_education.py
@@ -0,0 +1,74 @@
+import requests, re, json, copy
+from bs4 import BeautifulSoup
+import contentDeal
+
+# 获取页面超链接信息
+def get_html(url) :
+    html = requests.get(url)
+    html.encoding='utf-8'
+    soup = BeautifulSoup(html.text, 'lxml')
+    for i in soup.find_all('a', href=re.compile(r"/education/\d{4}[^/]*\.html$")) :
+        yield "http://www.helsinkitimes.fi" + i.get('href')
+# 获取超链接页面详细信息
+def get_html_link(link_list) :
+    # 创建字典
+    data = {'title': '', 'url': '','review': '', 'content': '', 'time': '', 'type': ''}
+    dataList = []
+    url_list = []    # 存储所有访问过的 url， 避免重复访问
+    # 遍历所有子链接
+    for i in link_list :
+        # 判断是否遍历过        
+        flag = 0
+        for url in url_list :
+            if url == str(i) :
+                flag = 1
+                break
+        if flag == 1 :
+            continue
+        url_list.append(i)
+        # 正常请求
+        html_link = requests.get(i)
+        html_link.encoding='utf-8'
+        soup = BeautifulSoup(html_link.text, 'lxml')
+        title = soup.title.string
+        url = i
+        review = ""
+        content_list = soup.select(".article-content-main")[0].select("p")
+        content = ""
+        for j in content_list :
+            content = content + str(j)
+        # 处理内容，替换字符串中的字符，去掉正文中的图片信息
+        content = contentDeal.deal_content(content)
+        # 通过标签名查找 时间 
+        time = soup.select("time")[0]
+        time = re.search(r"<time datetime=\"(.*)\">", str(time)).group(1)
+        type = "education"
+        # 给字典赋值
+        data['title'] = title
+        data['url'] = url
+        data['review'] = review
+        data['content'] = content
+        data['time'] = time
+        data['type'] = type
+        # 加入 List
+        dataList.append(data)
+        # 更改字典地址
+        data = copy.copy(data)
+    return dataList
+# 保存数据
+def save_data(content_list) :
+    with open('../www.helsinkitimes.fi/education.json', 'a', encoding='utf-8') as f:
+        for json_data in content_list :
+            f.write(json.dumps(json_data, ensure_ascii=False)+'\n')
+            f.flush()
+# 函数回调
+def fun_call(url) :
+    link_list = get_html(url)    # 返回一个生成器
+    content_list = get_html_link(link_list)
+    save_data(content_list)
+# 主函数
+def main() :
+    url = 'http://www.helsinkitimes.fi/themes/themes/education.html'
+    fun_call(url)
+if __name__=='__main__':
+    main()
